db029e3dbe4a1c7544ac0925b21cd40415fb9f15
E. Hannah <echannah631@gmail.com>
2025-10-29 20:43:15 -0400

    trying to cause action to run

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hey from Container!</h1>
+      <h1>Hey from the Container!</h1>
       <hr />
       <MarketingApp />
     </div>
